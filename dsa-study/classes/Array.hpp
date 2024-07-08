5e062750026ba2137631256f31bf33d43aa78c99
Anthony Tleiji <anthonytleiji@gmail.com>
2024-07-08 15:14:54 +0300

    Add add remove removeAt resize, print utility...

--- dsa-study/classes/Array.hpp
+++ dsa-study/classes/Array.hpp
@@ -3,45 +3,120 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <iostream>
 
-template <typename T, std::size_t N>
+template <typename T>
 class Array {
 public:
     Array();
-    T& operator[](std::size_t index);
-    const T& operator[](std::size_t index) const;
+    Array(int capacity);
+    ~Array();
+
     std::size_t size() const;
+    bool isEmpty() const;
+    T get(int index) const;
+    void set(int index, T value);
+    void clear();
+    void add(T elem);
+    void removeAt(int index);
+    void remove(T elem);
 
 private:
-    T data[N];
+    T* data;
+    int length;
+    int capacity;
+    
+private:
+    void resize(int newCapacity);
 };
 
-template <typename T, std::size_t N>
-Array<T, N>::Array() {
-    for (std::size_t i = 0; i < N; ++i) {
-        data[i] = T();
-    }
+template <typename T>
+Array<T>::Array() : length(0), capacity(16) {
+    data = new T[capacity];
 }
 
-template <typename T, std::size_t N>
-T& Array<T, N>::operator[](std::size_t index) {
-    if (index >= N) {
+template <typename T>
+Array<T>::Array(int capacity) : length(0), capacity(capacity) {
+    if (capacity <= 0) {
+        throw std::invalid_argument("Capacity must be greater than 0");
+    }
+    data = new T[capacity];
+}
+
+template <typename T>
+Array<T>::~Array() {
+    delete[] data;
+}
+
+template <typename T>
+std::size_t Array<T>::size() const {
+    return length;
+}
+
+template <typename T>
+bool Array<T>::isEmpty() const {
+    return length == 0;
+}
+
+template <typename T>
+T Array<T>::get(int index) const {
+    if (index < 0 || index >= length) {
         throw std::out_of_range("Index out of range");
     }
     return data[index];
 }
 
-template <typename T, std::size_t N>
-const T& Array<T, N>::operator[](std::size_t index) const {
-    if (index >= N) {
+template <typename T>
+void Array<T>::set(int index, T value) {
+    if (index < 0 || index >= length) {
         throw std::out_of_range("Index out of range");
     }
-    return data[index];
+    data[index] = value;
 }
 
-template <typename T, std::size_t N>
-std::size_t Array<T, N>::size() const {
-    return N;
+template <typename T>
+void Array<T>::clear() {
+    delete[] data;
+    data = new T[capacity];
+    length = 0;
+}
+
+template <typename T>
+void Array<T>::add(T elem) {
+    if (length >= capacity) {
+        resize(capacity * 2);
+    }
+    data[length++] = elem;
+}
+
+template <typename T>
+void Array<T>::removeAt(int index) {
+    if (index < 0 || index >= length) {
+        throw std::out_of_range("Index out of range");
+    }
+    for (int i = index; i < length - 1; ++i) {
+        data[i] = data[i + 1];
+    }
+    --length;
+}
+
+template <typename T>
+void Array<T>::remove(T elem) {
+    for (int i = 0; i < length; ++i) {
+        if (data[i] == elem) {
+            removeAt(i);
+            --i;
+        }
+    }
+}
+
+template <typename T>
+void Array<T>::resize(int newCapacity) {
+    T* newData = new T[newCapacity];
+    std::copy(data, data + length, newData);
+    delete[] data;
+    data = newData;
+    capacity = newCapacity;
 }
 
 #endif /* Array_hpp */
--- dsa-study/main.cpp
+++ dsa-study/main.cpp
@@ -2,22 +2,29 @@
 #include "classes/Test.hpp"
 #include "classes/Array.hpp"
 
-int main(int argc, const char * argv[]) {
-    Test test;
-    test.sayHello();
-    
+template <typename T>
+void printArray(const Array<T>& arr) {
+    for (std::size_t i = 0; i < arr.size(); ++i) {
+        std::cout << arr.get(i) << " ";
+    }
     std::cout << std::endl;
-    
-    Array<int, 5> arr;
-    for (std::size_t i = 0; i < arr.size(); ++i) {
-            arr[i] = static_cast<int>(i) * 10;
-        }
+}
 
-    for (std::size_t i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i] << " ";
+int main() {
+    Array<int> arr(5);
+
+    for (int i = 0; i < 5;) {
+        arr.add(++i);
     }
 
-    std::cout << std::endl;
+    printArray(arr);
     
+    std::cout << std::endl;
+    std::cout << "Array size: " << arr.size() << std::endl;
+    std::cout << "Array third element : " << arr.get(2) << std::endl;
+    std::cout << "Array is empty: " << (arr.isEmpty() ? "Yes" : "No") << std::endl;
+    arr.clear();
+    std::cout << "Array is empty after clear: " << (arr.isEmpty() ? "Yes" : "No") << std::endl;
+
     return 0;
 }
